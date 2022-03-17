4831d035611c1b0739bd72355fcb9dd259a2c777
leigh966 <leigh966@hotmail.com>
2022-03-17 11:12:27 +0000

    Made outputs more informative

--- List Comprehension.py
+++ List Comprehension.py
@@ -1,11 +1,11 @@
 def doubleExample():
     list1 = [2, 3]
     doubled = [num*2 for num in list1]
-    print(doubled)
+    print(list1, " doubled is ", doubled)
 doubleExample()
 
 def startsWithExample():
     friends = ["Rolf", "Sally", "Sam"]
     starts_with_S = [friend  for friend in friends  if friend.startswith("S")]
-    print(starts_with_S)
+    print("Names in ", friends," starting with 'S': ", starts_with_S)
 startsWithExample()
