8377d6e60cb57c70ebc7bdc1c327b5f305116855
leigh966 <leigh966@hotmail.com>
2022-03-17 10:26:52 +0000

    Seperated difference example into it's own function and created a union example

--- Advanced Set Operations.py
+++ Advanced Set Operations.py
@@ -1,5 +1,17 @@
-friends = {"Charlie", "Bob", "Adam"}
-abroad = {"Charlie", "Bob"}
+def differenceExample():
+    friends = {"Charlie", "Bob", "Adam"}
+    abroad_friends = {"Charlie", "Bob"}
 
-local_friends = friends.difference(abroad) # friends - abroad
-print("Friends that are not abroad: ", local_friends)
+    local_friends = friends.difference(abroad_friends) # friends - abroad
+    print("Friends that are not abroad: ", local_friends)
+
+def unionExample():
+    friends_that_wear_glasses = {"Charlie", "Bob"}
+    friends_that_drive = {"Adam", "Bob"}
+
+    all_friends = friends_that_wear_glasses.union(friends_that_drive) 
+    # friends_that_wear_glasses + friends_that_drive
+    print("All friends: ", all_friends) # Elements are still unique
+
+differenceExample()
+unionExample()
