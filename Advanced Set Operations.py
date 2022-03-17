ba44cc471ce76b2d5c579e6b407a2915ce4986ab
leigh966 <leigh966@hotmail.com>
2022-03-17 10:31:03 +0000

    Added intersection example

--- Advanced Set Operations.py
+++ Advanced Set Operations.py
@@ -4,6 +4,7 @@ def differenceExample():
 
     local_friends = friends.difference(abroad_friends) # friends - abroad
     print("Friends that are not abroad: ", local_friends)
+differenceExample()
 
 def unionExample():
     friends_that_wear_glasses = {"Charlie", "Bob"}
@@ -12,6 +13,13 @@ def unionExample():
     all_friends = friends_that_wear_glasses.union(friends_that_drive) 
     # friends_that_wear_glasses + friends_that_drive
     print("All friends: ", all_friends) # Elements are still unique
-
-differenceExample()
 unionExample()
+
+
+def intersectionExample():
+    art = {"Charlie", "Bob", "Adam", "Nick"}
+    science = {"Jen", "Chloe", "Charlie", "Adam"}
+
+    both = art.intersection(science)
+    print("People who study both art and science: ", both)
+intersectionExample()
