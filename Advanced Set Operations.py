28fd45cdcd08204f3029133bf20ff1b8ca977a8b
leigh966 <leigh966@hotmail.com>
2022-03-17 10:19:29 +0000

    Comment to explain how .difference works

--- Advanced Set Operations.py
+++ Advanced Set Operations.py
@@ -1,5 +1,5 @@
 friends = {"Charlie", "Bob", "Adam"}
 abroad = {"Charlie", "Bob"}
 
-local_friends = friends.difference(abroad)
+local_friends = friends.difference(abroad) # friends - abroad
 print("Friends that are not abroad: ", local_friends)
