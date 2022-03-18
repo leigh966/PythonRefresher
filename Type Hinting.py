cd165eb99294d1ec0848df34dbba2bb3723cfdc5
leigh966 <leigh966@hotmail.com>
2022-03-18 10:34:22 +0000

    Added note comment about linters on type hinting example

--- Type Hinting.py
+++ Type Hinting.py
@@ -4,4 +4,6 @@ from typing import List
 def list_avg(sequence: List) -> float:
     return sum(sequence) / len(sequence)
 
+# This will show up as an issue
+# if you are using a linter
 list_avg(123)
